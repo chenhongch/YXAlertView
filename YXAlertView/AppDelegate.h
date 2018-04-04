0d82a4a3afb62c62b039cbf5fa05be94412402c5
jokechen <jokechen@luosongdemacbook-pro.local>
2018-04-04 11:03:26 +0800

    Initial Commit

---
+++ YXAlertView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YXAlertView
+//
+//  Created by jokechen on 2018/4/4.
+//  Copyright © 2018年 陈红. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YXAlertView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YXAlertView
+//
+//  Created by jokechen on 2018/4/4.
+//  Copyright © 2018年 陈红. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
